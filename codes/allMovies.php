d8891d93d7d27cd9380a818b7ee7aad5eab9b51c
Chukwunonso <offorma@gmail.com>
2016-03-20 16:34:16 +0000

    0

--- codes/allMovies.php
+++ codes/allMovies.php
@@ -10,7 +10,7 @@
 $sql = "SELECT * FROM marvelmovies";
 $result = mysqli_query($conn,$sql);
 
-while($row = mysqli_fetch_array($result))
+while($row = mysqli_fetch_row($result))
 {
     /* the code inside here is repeated for each item in the array
     You can do things like the following to print out each movie title */
