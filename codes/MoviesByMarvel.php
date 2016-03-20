0d8dde9831ab3c9fccd12d14ca7ebef0a3450296
Chukwunonso <offorma@gmail.com>
2016-03-20 22:50:09 +0000

    0

--- codes/MoviesByMarvel.php
+++ codes/MoviesByMarvel.php
@@ -16,7 +16,7 @@
      */
     include 'server.inc.php';//include php file where i have connection to db
 
-    $sql = "SELECT title, yearReleased, productionStudio FROM marvelmovies WHERE productionStudio = Marvel Studio";//create the sql query string
+    $sql = "SELECT title, yearReleased, productionStudio FROM marvelmovies WHERE productionStudio = 'Marvel Studio'";//create the sql query string
 
     $result = mysqli_query($conn,$sql);//Query the db using the mysqli_query function and store result in result varaible.
 
