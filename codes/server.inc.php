fa1bd5e28829c0f9bf03f53cb460475ad646c4b4
Chukwunonso <offorma@gmail.com>
2016-03-20 16:33:22 +0000

    0

--- codes/server.inc.php
+++ codes/server.inc.php
@@ -9,9 +9,9 @@
 $servername = "ap-cdbr-azure-east-c.cloudapp.net";
 $username = "bccb84f33826f0";
 $password = "7b0e4bef";
-
+$db = "Nonso1514414";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $db);
 
 // Check connection
 if (!$conn) {
