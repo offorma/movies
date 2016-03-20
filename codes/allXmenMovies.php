94fd9ff11159f3b9d4697d16c8aa2aaa5b3485d9
Chukwunonso <offorma@gmail.com>
2016-03-20 23:03:19 +0000

    0

--- codes/allXmenMovies.php
+++ codes/allXmenMovies.php
@@ -15,12 +15,10 @@
      * Time: 22:13
      */
     include 'server.inc.php';//include php file where i have connection to db
-echo "c";
     $sql = "SELECT title, yearReleased, productionStudio FROM marvelmovies WHERE title LIKE '%x-men%'";//create the sql query string
 
     $result = mysqli_query($conn,$sql);//Query the db using the mysqli_query function and store result in result varaible.
 
-    echo "d";
     while($row = mysqli_fetch_assoc($result)){
 
 
